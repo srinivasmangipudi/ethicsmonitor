c3c0e731ed69391ad457723f868e40b875d7f4ca
Srinivas Mangipudi <shrinimann@gmail.com>
2014-12-08 23:42:49 +0530

    Comment opinion is now being saved with UI

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,5 +1,17 @@
 Template.commentItem.helpers({
 	submittedText: function() {
 		return new Date(this.submitted).toString();
+	},
+
+	myOpinion: function() {
+		if(this.opinion === "yes")
+			//return "comment-opinion-yes";
+			return true;
+		else if(this.opinion === "no")
+			//return "comment-opinion-no";
+			return false;
+		else
+			//return "comment-opinion-no";
+			return false;
 	}
 });
--- client/templates/comments/comment_submit.js
+++ client/templates/comments/comment_submit.js
@@ -5,12 +5,24 @@ Template.commentSubmit.created = function() {
 Template.commentSubmit.helpers({
 	errorMessage: function(field) {
 		return Session.get('commentSubmitErrors')[field];
-		//return '';
 	},
 
 	errorClass: function(field) {
 		return !!Session.get('commentSubmitErrors')[field] ? 'has-error': '';
-		//return '';
+	},
+
+	opinionYesClass: function() {
+		if(Session.get('commentOpinion') === "yes")
+			return 'btn-success active';
+		else
+			return 'btn-default';
+	},
+
+	opinionNoClass: function() {
+		if(Session.get('commentOpinion') === "no")
+			return 'btn-danger active';
+		else
+			return 'btn-default';
 	}
 });
 
@@ -22,12 +34,18 @@ Template.commentSubmit.events({
 
 		var comment = {
 			body: $body.val(),
-			dilemmaId: template.data._id
+			dilemmaId: template.data._id,
+			opinion: Session.get('commentOpinion')
 		};
 
 		var errors = {};
 		if(! comment.body) {
 			errors.body = "Please write some content";
+			return Session.set('commentSubmitErrors', errors);
+		}
+
+		if(typeof comment.opinion == 'undefined') {
+			errors.body = "Please select 'Yes' or 'No'";
 			return Session.set('commentSubmitErrors', errors);
 		}
 
@@ -38,5 +56,16 @@ Template.commentSubmit.events({
 				$body.val('');
 			}
 		});
+	},
+
+	'click .opinion-yes': function(e) {
+		e.preventDefault();
+		return Session.set('commentOpinion', "yes");
+	},
+
+	'click .opinion-no': function(e) {
+		e.preventDefault();
+
+		return Session.set('commentOpinion', "no");
 	}
 });
