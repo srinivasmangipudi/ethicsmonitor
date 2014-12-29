af2d627e21f45035663b7ba732943e0997824fc1
Srinivas Mangipudi <shrinimann@gmail.com>
2014-12-29 17:06:36 +0530

    extracted feedback as a common element and included it on the dilemma list page also

---
+++ client/templates/application/feedback_box.js
@@ -0,0 +1,57 @@
+Template.feedbackBox.rendered = function() {
+    Session.set('feedbackGiven', false);
+};
+
+Template.feedbackBox.helpers({
+	feedbackGiven: function() {
+		return Session.get('feedbackGiven');
+	}
+});
+
+Template.feedbackBox.events({
+	'submit form': function(e, template) {
+		e.preventDefault();
+
+		var user = Meteor.user();
+		var from = '';
+
+		if(user)
+			{
+				from = contactEmail(user);
+				user = user._id;
+			}
+			
+		else
+			user = "Not Logged User"
+
+		if(!from)
+			from = "userwithnoemail@ethicsmonitor.org";
+
+		var to = "em@goa-cap.org";
+
+		var $body = $(e.target).find('[name=feedback]');
+		var subject = "Feedback/Suggestion from: " + user || undefined;
+
+		// In your client code: asynchronously send an email
+		Meteor.call('sendEmail',
+            to,
+            from,
+            subject,
+            $body.val());
+
+		Session.set('feedbackGiven', true);
+	},
+
+	'click .newSuggestion':function(){
+    	//console.log("new suggestion clicked");
+    	Session.set('feedbackGiven', false);
+  },
+});
+
+var contactEmail = function (user) {
+  if (user.emails && user.emails.length)
+    return user.emails[0].address;
+  if (user.services && user.services.facebook && user.services.facebook.email)
+    return user.services.facebook.email;
+  return null;
+};
--- client/templates/application/landing_page.js
+++ client/templates/application/landing_page.js
@@ -1,5 +1,4 @@
 Template.landingPage.rendered = function() {
-    Session.set('feedbackGiven', false);
 };
 
 Template.landingPage.helpers({
@@ -10,55 +9,7 @@ Template.landingPage.helpers({
 	lastDilemmaCredits: function() {
 		return this.dbGems.lastImageDilemmaCredits;
 	},
-	feedbackGiven: function() {
-		return Session.get('feedbackGiven');
-	}
 });
 
 Template.landingPage.events({
-	'submit form': function(e, template) {
-		e.preventDefault();
-
-		var user = Meteor.user();
-		var from = '';
-
-		if(user)
-			{
-				from = contactEmail(user);
-				user = user._id;
-			}
-			
-		else
-			user = "Not Logged User"
-
-		if(!from)
-			from = "userwithnoemail@ethicsmonitor.org";
-
-		var to = "em@goa-cap.org";
-
-		var $body = $(e.target).find('[name=feedback]');
-		var subject = "Feedback/Suggestion from: " + user || undefined;
-
-		// In your client code: asynchronously send an email
-		Meteor.call('sendEmail',
-            to,
-            from,
-            subject,
-            $body.val());
-
-		Session.set('feedbackGiven', true);
-	},
-
-	'click .newSuggestion':function(){
-    	//console.log("new suggestion clicked");
-    	Session.set('feedbackGiven', false);
-  },
 });
-
-var contactEmail = function (user) {
-  if (user.emails && user.emails.length)
-    return user.emails[0].address;
-  if (user.services && user.services.facebook && user.services.facebook.email)
-    return user.services.facebook.email;
-  return null;
-};
