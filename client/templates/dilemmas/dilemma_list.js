90264bf6375995a83f6e002eff900b6665cc6e5f
Srinivas Mangipudi <shrinimann@gmail.com>
2015-01-21 14:55:53 +0530

    created custom banner/page for goaphoto

--- client/templates/dilemmas/dilemma_list.js
+++ client/templates/dilemmas/dilemma_list.js
@@ -1,5 +1,7 @@
 Template.dilemmasList.rendered = function() {
     //console.log(this.data);
+    //var controller = Router.current();
+    //console.log(controller.params.tagName);
 };
 
 Template.dilemmasList.helpers({
@@ -8,6 +10,14 @@ Template.dilemmasList.helpers({
 			dilemma._rank = index;
 			return dilemma;
 		});
+	},
+
+	customPage: function() {
+		var controller = Router.current();
+		if(controller.params.tagName && controller.params.tagName.toUpperCase() === 'GOAPHOTO')
+			return 'https://s3.amazonaws.com/ethicsmonitor/siteimages/goaphoto.png';
+		else
+			return false;
 	}
 	/*latestDilemma: function() {
 		console.log(this.myLatestDilemma);
